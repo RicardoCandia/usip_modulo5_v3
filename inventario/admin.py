8e629e4b6380ec0b4996dd40408948294cf27106
marcelo.arteaga <marcelo.arteaga@elevatesecurity.com>
2023-06-07 22:37:09 -0400

    dia 2

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Categoria
+from .models import Producto
 
 # Register your models here.
+"""
+CATEGORIA
+"""
+admin.site.register(Categoria)
+
+"""
+PRODUCTO
+"""
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'precio', 'disponible')
+    list_filter = ('categoria', 'disponible')
+    search_fields = ('nombre', 'categoria__nombre')
+    ordering = ('nombre', 'precio')
+
+admin.site.register(Producto, ProductoAdmin)
