65230ec0fd0bff96470240f611ed1b0579469a7d
marcelo.arteaga <marcelo.arteaga@elevatesecurity.com>
2023-06-13 21:43:59 -0400

    dia 3

--- inventario/urls.py
+++ inventario/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("contact/<str:name>", views.contact),
+    path("categorias/", views.categorias),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Categoria
 
 
 def index(request):
     return HttpResponse("Hello, world")
+
+def contact(request, name):
+    return HttpResponse(f"Bienvenido {name} a la clase de Django")
+
+def categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, "categorias.html", {
+        "categorias": categorias
+    })
