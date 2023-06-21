6dd93cf085261a9dbc029d1b5294e6969e7dd983
marcelo.arteaga <marcelo.arteaga@elevatesecurity.com>
2023-06-20 20:25:55 -0400

    dia 6

--- inventario/serializers.py
+++ inventario/serializers.py
@@ -16,3 +16,8 @@ class ProductoSerializer(serializers.ModelSerializer):
 class ReporteProductosSerializer(serializers.Serializer):
     cantidad = serializers.IntegerField()
     productos = ProductoSerializer(many=True)
+
+class ContactSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    subject = serializers.CharField(max_length=200)
+    body = serializers.CharField(max_length=500)
--- inventario/validators.py
+++ inventario/validators.py
@@ -13,3 +13,10 @@ def validation_categoria(value):
         raise ValidationError(
             "No es una opcion permitida"
         )
+        
+def validar_nombre(value):
+    if value == "Comida":
+        raise ValidationError(
+            "%(value)s no es un texto permitido",
+            params={"value": value},
+        )
